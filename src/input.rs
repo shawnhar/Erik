2c1e3d52321c87ba446b60267064eb292ea2199f
shawnhar@microsoft.com <shawnhar@microsoft.com>
2021-06-14 16:42:39 -0700

    Add unit tests for argument selection

--- src/input.rs
+++ src/input.rs
@@ -37,11 +37,74 @@ impl InputSource {
 }
 
 
+// We can iterate over the input source, which will yield a series of strings.
 impl IntoIterator for InputSource {
     type Item = String;
     type IntoIter = std::vec::IntoIter<Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.source_text.into_iter()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_args() {
+        let input = InputSource::new(vec![]);
+        let mut iter = input.into_iter();
+
+        assert!(iter.next() == None);
+    }
+
+    #[test]
+    fn test_one_arg() {
+        let input = InputSource::new(vec![ String::from("Hello") ]);
+        let mut iter = input.into_iter();
+
+        assert_eq!(iter.next().unwrap(), String::from("Hello"));
+        assert!(iter.next() == None);
+    }
+
+    #[test]
+    fn test_two_args() {
+        let input = InputSource::new(vec![ String::from("Hello"), String::from("World") ]);
+        let mut iter = input.into_iter();
+
+        assert_eq!(iter.next().unwrap(), String::from("Hello"));
+        assert_eq!(iter.next().unwrap(), String::from("World"));
+        assert!(iter.next() == None);
+    }
+
+    #[test]
+    fn test_one_arg_file_exists() {
+        fs::write("args.txt", "This\nis a\ntest").unwrap();
+
+        let input = InputSource::new(vec![ String::from("args.txt") ]);
+        let mut iter = input.into_iter();
+
+        assert_eq!(iter.next().unwrap(), String::from("This"));
+        assert_eq!(iter.next().unwrap(), String::from("is a"));
+        assert_eq!(iter.next().unwrap(), String::from("test"));
+        assert!(iter.next() == None);
+        
+        fs::remove_file("args.txt").unwrap();
+    }
+
+    #[test]
+    fn test_two_args_file_exists() {
+        fs::write("args2.txt", "This\nis a\ntest").unwrap();
+
+        let input = InputSource::new(vec![ String::from("args.txt"), String::from("another") ]);
+        let mut iter = input.into_iter();
+
+        assert_eq!(iter.next().unwrap(), String::from("args.txt"));
+        assert_eq!(iter.next().unwrap(), String::from("another"));
+        assert!(iter.next() == None);
+        
+        fs::remove_file("args2.txt").unwrap();
     }
 }
